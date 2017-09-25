f3c3feca68709b67d5e1b81808b624c3c5e14757
ninuxGithub <1732768613@qq.com>
2017-09-25 08:52:35 +0800

    m

--- spring-boot-security-oauth2-server/src/main/java/com/example/demo/repository/ClientRepository.java
+++ spring-boot-security-oauth2-server/src/main/java/com/example/demo/repository/ClientRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.stereotype.Repository;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.security.oauth2.provider.ClientDetails;
+//import org.springframework.stereotype.Repository;
 
 //@Repository
 //public interface ClientRepository extends JpaRepository<T, Serializable> {
